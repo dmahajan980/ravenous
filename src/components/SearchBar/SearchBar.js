2f013300e256c1ca570c189e024c82c877b4a573
Divyanshu Mahajan <divyanshumahajan98@gmail.com>
2019-06-23 01:12:51 +0700

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -21,7 +21,7 @@ class SearchBar extends React.Component {
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions}
+                        {this.renderSortByOptions()}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
